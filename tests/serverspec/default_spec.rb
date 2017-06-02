2b3243cb8a13ed74dfef14230104ebd73581bcd8
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-06-02 17:06:41 +0900

    QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,42 +1,40 @@
-require 'spec_helper'
+require "spec_helper"
 
-package = 'ntp'
-service = 'ntp'
-config  = '/etc/ntp.conf'
-db_dir  = '/var/lib/ntp'
+package = "ntp"
+service = "ntp"
+config  = "/etc/ntp.conf"
+db_dir  = "/var/lib/ntp"
 
 case os[:family]
-when 'freebsd'
-  db_dir = '/var/db/ntp'
-  service = 'ntpd'
-when 'redhat'
-  service = 'ntpd'
+when "freebsd"
+  db_dir = "/var/db/ntp"
+  service = "ntpd"
+when "redhat"
+  service = "ntpd"
 end
 puts os[:family]
 
-leap_file = "#{ db_dir }/leap-seconds.list"
+leap_file = "#{db_dir}/leap-seconds.list"
 
-case os[:family]
-when 'freebsd'
-else
+unless os[:family] == "freebsd"
   describe package(package) do
     it { should be_installed }
-  end 
+  end
 end
 
 case os[:family]
-when 'redhat'
-  describe package('libselinux-python') do
+when "redhat"
+  describe package("libselinux-python") do
     it { should be_installed }
   end
 end
 
 describe file(config) do
   it { should be_file }
-  its(:content) { should match Regexp.escape('server 0.pool.ntp.org') }
-  its(:content) { should match Regexp.escape('server 1.pool.ntp.org') }
-  its(:content) { should match Regexp.escape('server 2.pool.ntp.org') }
-  its(:content) { should match Regexp.escape('server 3.pool.ntp.org') }
+  its(:content) { should match Regexp.escape("server 0.pool.ntp.org") }
+  its(:content) { should match Regexp.escape("server 1.pool.ntp.org") }
+  its(:content) { should match Regexp.escape("server 2.pool.ntp.org") }
+  its(:content) { should match Regexp.escape("server 3.pool.ntp.org") }
 end
 
 describe file(leap_file) do
